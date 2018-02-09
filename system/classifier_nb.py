f9ee9f53fe590dfa743c5e381655e346fd5b5292
srv-twry <saurav007tiwary@gmail.com>
2018-02-09 22:50:34 +0530

    feat: Implemented using GaussianNB Not well implemented, needs cleaning up 30% accuracy - Extremely poor

--- system/classifier_nb.py
+++ system/classifier_nb.py
@@ -15,9 +15,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset_size = 10000
+dataset_size = 5000
 dataset = pd.read_csv('dataset/dataset.csv')
 dataset = dataset.iloc[0:dataset_size,:]
+
+# Checking for missing values
+dataset.isnull().any()
 
 """
    Data Preprocessing. 
@@ -49,6 +52,38 @@ for i in range(0, dataset_size):
     body = [ps.stem(word) for word in body if not word in set(stopwords.words('english'))]
     body = ' '.join(body)
     body_corpus.append(body)
+    
+# Creating the bag of words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X_title = cv.fit_transform(title_corpus).toarray()
+X_body = cv.fit_transform(body_corpus).toarray()
+X = np.concatenate((X_title, X_body, dataset.iloc[:,4:6]), axis = 1)
+y = dataset.iloc[:,-1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+
+
+"""
+    Training and Testing.
+"""
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# Getting the percentage correct predictions
+np.mean(y_pred == y_test)
+    
+
     
     
 
